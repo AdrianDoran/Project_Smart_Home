5f253a52ee8f9412e5dc5c01da48783e3daf95c1
bhodgs <bhodgs@users.noreply.github.com>
2019-08-29 15:42:09 +1000

    NEW: Server.js updated with necessities.

---
+++ web/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
+const base = `${__dirname}/public`;
+app.use(express.static('public'));
+
+app.use((req, res, next) =>
+{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`);
+})
+
+app.get('/', function(req, res) {
+    console.log(`Index loading...`);
+    res.sendFile(`${base}/index.html`);
+})
+
