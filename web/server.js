1ff406dc0b8a1935d251bb9feaa129812f3b4994
AdrianDoran <AdrianDoran@users.noreply.github.com>
2019-08-31 13:23:48 +0800

    DOC: Add Comments to Web Files
    Added Comments to Server and HTML Files

--- web/server.js
+++ web/server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8080; // Dedicated Port
 const base = `${__dirname}/public`;
 app.use(express.static('public'));
 
+// CORS Header
 app.use((req, res, next) =>
 {
     res.header("Access-Control-Allow-Origin", "*");
@@ -11,15 +12,18 @@ app.use((req, res, next) =>
     next();
 });
 
+// Listen for port being used, Port is 8080 if cannot connect
 app.listen(port, () => {
     console.log(`Listening on port: ${port}`);
 })
 
+// Get Index file of Web Applcation
 app.get('/', function(req, res) {
     console.log(`Index loading...`);
     res.sendFile(`${base}/index.html`);
 })
 
+// Get Error file when error is detected
 app.get('*', (req, res) => {
     console.log('Loading 404.');
     res.sendFile(`${base}/404.html`);
