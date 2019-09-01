f19a48d24b0fcd71eb0a0a3d687431415a94e110
bhodgs <bhodgs@deakin.edu.au>
2019-09-01 23:36:08 +1000

    NEW: Full logon service with registration.

--- web/app/src/app/app.component.ts
+++ web/app/src/app/app.component.ts
@@ -1,31 +1,27 @@
-import { Component, NgModule } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
-
-
+import { AuthenticationService } from './_services';
+import { User } from './_models';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-
 export class AppComponent {
-  title = 'app';
-  user= {
-    username: "null",
-    password: "null"
+  currentUser: User;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+      this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
   }
-  json;
-  authenticate(username, password){ 
-    this.user.password = password;
-    this.user.username = username;}
-    constructor(private http: HttpClient,){
-      this.http.post("localhost:5000/api/authenticate", JSON.stringify(this.user)).toPromise().then((data:any) =>  {
-        console.log(data)
-        this.json = JSON.stringify(data.json);
-      });
-    }
-    
-    
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
+}
+
 }
