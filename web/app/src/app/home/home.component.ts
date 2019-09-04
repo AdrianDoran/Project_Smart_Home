decbd8a1cdff04fb19ced123afb6e7e5b955c091
bhodgs <bhodgs@users.noreply.github.com>
2019-09-04 13:08:01 +1000

    NEW: Device field.

--- web/app/src/app/home/home.component.ts
+++ web/app/src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   public currentUser;
+  private userDevice;
   constructor() {
     this.currentUser = localStorage.getItem('currentUser')? JSON.parse(localStorage.getItem('currentUser')) : '';
    }
