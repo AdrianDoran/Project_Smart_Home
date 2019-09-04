7a667b1710a3a33fa579ff511bfb1f733a4a403c
bhodgs <bhodgs@users.noreply.github.com>
2019-09-04 13:43:16 +1000

    FIX: Clear HTML structure for navbar.

--- web/app/src/app/_models/device.ts
+++ web/app/src/app/_models/device.ts
@@ -1,7 +1,7 @@
 export class Device{
     email: String;
     name: String;
-    data: Array<Int16Array>; // May need changing according to the data.
+    data: Array<Int16Array>; // May need changing according to the data. This should not be the main portion of the data -> just for local storage.
     
 
 }
