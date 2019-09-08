318894943394d2ae07091a5a740c407c3c9d28f8
bhodgs <bhodgs@users.noreply.github.com>
2019-09-08 14:27:22 +1000

    FIX: Button selection for device.

--- web/app/src/app/home/home.component.ts
+++ web/app/src/app/home/home.component.ts
@@ -12,26 +12,33 @@ import { AuthenticationService } from '../_services';
 export class HomeComponent implements OnInit {
   public currentUser;
   public userDevices;
+  public currentDevice;
   constructor(
-      private formBuilder: FormBuilder,
-      private route: ActivatedRoute,
-      private router: Router,
-      private authenticationService : AuthenticationService,
-      private toastr: ToastrService
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private toastr: ToastrService
   ) {
-    this.currentUser = localStorage.getItem('currentUser')? JSON.parse(localStorage.getItem('currentUser')) : '';
-    this.userDevices = localStorage.getItem('userDevices')? JSON.parse(localStorage.getItem('userDevices')) : '';
-   }
+    this.currentUser = localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')) : '';
+    this.userDevices = localStorage.getItem('userDevices') ? JSON.parse(localStorage.getItem('userDevices')) : '';
+  }
 
   ngOnInit() {
     this.authenticationService.getDevices(this.currentUser.email)
-       .subscribe(
-           data => {
-            this.toastr.success("Devices retrieved.");
-           },
-           error => {
-             this.toastr.info("You don't have any devices added.");
-           });
+      .subscribe(
+        data => {
+          this.toastr.success("Devices retrieved.");
+        },
+        error => {
+          this.toastr.info("You don't have any devices added.");
+        });
+
+
   }
 
+  selectDevice(device: string) {
+    this.currentDevice = device;
+    localStorage.setItem('currentDevice', this.currentDevice);
+  }
 }
