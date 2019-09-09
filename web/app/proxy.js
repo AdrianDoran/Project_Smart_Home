83624cbda6187a307e9ea03754229817c1f1991e
bhodgs <bhodgs@users.noreply.github.com>
2019-09-09 12:49:58 +1000

    NEW: Added now.sh as base url.

--- web/app/proxy.js
+++ web/app/proxy.js
@@ -1,7 +1,7 @@
 var HttpsProxyAgent = require('https-proxy-agent');
 var proxyConfig = [{
   context: '/api',
-  target: 'localhost:5000',
+  target: 'https://affordable-smarthome.now.sh',
   secure: false
 }];
 
