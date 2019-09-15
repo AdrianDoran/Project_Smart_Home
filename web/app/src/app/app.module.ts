2d1adff727152e66e7f5d801996952c5d60692ce
AdrianDoran <AdrianDoran@users.noreply.github.com>
2019-09-15 16:55:14 +0800

    NEW: Add Sticky Footer for data rep

--- web/app/src/app/app.module.ts
+++ web/app/src/app/app.module.ts
@@ -10,7 +10,6 @@ import { InterceptorService } from './_services/interceptor.service';
 import { UserService } from './_services/user.service';
 import { DeviceService } from './_services/device.service';
 import { DeviceDataService } from './_services/device.data.service';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -19,6 +18,7 @@ import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './navbar/navbar.component';
 import { AddDeviceComponent } from './adddevice/add.device.component';
 import { DeviceListComponent } from './devicelist/device.list.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -29,7 +29,8 @@ import { DeviceListComponent } from './devicelist/device.list.component';
     AddDeviceComponent,
     NavBarComponent,
     HomeComponent,
-    DeviceListComponent
+    DeviceListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
