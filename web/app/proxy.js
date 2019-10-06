df11230d881775a63178ea87f00abe5babf8fa65
bhodgs <bhodgs@users.noreply.github.com>
2019-10-06 18:30:58 +1100

    Updates to now endpoints

--- web/app/proxy.js
+++ web/app/proxy.js
@@ -1,7 +1,7 @@
 var HttpsProxyAgent = require('https-proxy-agent');
 var proxyConfig = [{
   context: '/api',
-  target: 'http://localhost:5000/',
+  target: 'https://affordable-smarthome.bhodgs.now.sh/',
   secure: false
 }];
 
